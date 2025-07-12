cc24813b79146bb221f07a2db9dc44ae727ded7c
Mahmoud-7-4 <m.ebrahim1148@su.edu.eg>
2025-07-12 18:35:32 +0300

    c#03

---
+++ intro C# 01/Demo Session 02/Demo 02/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Demo_02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region (Fractions Or Float Point) And Discard
+
+            float floatNumber = 1.123456f;
+            Console.WriteLine(floatNumber);
+
+            #endregion
+        }
+    }
+}
